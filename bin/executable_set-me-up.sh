a5979b58c024290ea5048db0b80ebed875ba6e9d
Mannu Silva <wise.tent4987@fastmail.com>
2022-05-02 20:01:21 -0300

    initial commit

---
+++ bin/executable_set-me-up.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+###############################################
+# Sets up Homebrew and Brewable things.       #
+###############################################
+
+#Installs Homebrew if not installed yet.
+if ! command -v brew &> /dev/null
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+#Installs all Homebrew apps necessary
+homebrew_apps=( 
+                coreutils 
+                dockutil 
+                mysides 
+                chezmoi 
+                mas 
+                "--cask 1password/tap/1password-cli" 
+)
+
+for homebrew_app in "${homebrew_apps[@]}"
+do
+  if ! brew ls $homebrew_app &> /dev/null
+  then
+    brew install $homebrew_app
+  fi
+done
+
+###############################################
+# Installs MacApp Store Apps                  #
+###############################################
+macstore_apps=( 
+                402670023  # Keyboard Pilot
+                425424353  # The Unarchiver
+                1508732804 # Soulver
+                403504866  # PCalc
+                937984704  # Amphetamine
+)
+
+for macstore_app in "${macstore_apps[@]}"
+do
+  mas install $macstore_app
+done
+
+###############################################
+# Sets up unlinked github repos.              #
+###############################################
+github_repos=(
+                "trapd00r/LS_COLORS"
+)
+for github_repo in "${github_repos[@]}"
+do
+  dirname=`echo $github_repo | cut -d '/' -f2`
+  if ! test -d ~/.local/share/$dirname &> /dev/null
+  then
+    git clone https://github.com/$github_repo ~/.local/share/$dirname
+  fi
+done
+
+###############################################
+# Everything Else                             #
+###############################################
+if ! test -f ~/.iterm2_shell_integration.zsh
+then
+  curl -L https://iterm2.com/shell_integration/install_shell_integration.sh | bash
+fi
